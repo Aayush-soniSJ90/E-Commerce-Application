8c75c567b518dddf12261a46f4d4fe84d7155ec1
Ayush Soni <145642197+Aayush-soniSJ90@users.noreply.github.com>
2024-09-13 15:22:49 +0530

    EurekaClient uploaded

---
+++ ServiceDiscoveryEurekaClient/ServiceDiscoveryEurekaClient/src/main/java/dev/ayush/ServiceDiscoveryEurekaClient/ServiceDiscoveryEurekaClientApplication.java
@@ -0,0 +1,15 @@
+package dev.ayush.ServiceDiscoveryEurekaClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceDiscoveryEurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceDiscoveryEurekaClientApplication.class, args);
+	}
+
+}
---
+++ ServiceDiscoveryEurekaClient/ServiceDiscoveryEurekaClient/src/test/java/dev/ayush/ServiceDiscoveryEurekaClient/ServiceDiscoveryEurekaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ayush.ServiceDiscoveryEurekaClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryEurekaClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
